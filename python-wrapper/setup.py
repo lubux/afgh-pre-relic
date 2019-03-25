9d1db138c692e5d65e673c405a49a34b32530618
Ed Oakes <ed.nmi.oakes@gmail.com>
2019-03-25 17:08:42 +0000

    Used shared library in python wrapper

--- python-wrapper/setup.py
+++ python-wrapper/setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup, Extension
 
 pypre = Extension('pypre',
-                libraries = ['relic'],
+                libraries = ['pre-afgh-relic', 'relic'],
                 runtime_library_dirs = ['/usr/local/lib', '/opt/lib'],
-                sources = ['pre_python.c', '../pre/encoding.c', '../pre/encryption.c', '../pre/keygen.c', '../pre/utils.c'])
+                sources = ['pre_python.c'])
 
 setup(name='pypre', ext_modules=[pypre])
