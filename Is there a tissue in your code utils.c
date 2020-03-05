12a8ffcc6b59e1fbe7b6680c8f0bdd623286636c
cbyisclever <60684927+cbyisclever@users.noreply.github.com>
2020-03-05 22:17:34 +0800

    Create Is there a tissue in your code utils.c
    try to explain a bug

---
+++ Is there a tissue in your code utils.c
@@ -0,0 +1,7 @@
+in utils.c line 84,
+the function is: md_kdf2(key, key_len, buffer, (int)buff_size);
+but error while build,according to relic_md.h,
+it should be md_kdf(key, key_len, buffer, (int)buff_size);
+? is the version of relic cause the problem?
+sorry to bother u guys!
+good wishes for u.
